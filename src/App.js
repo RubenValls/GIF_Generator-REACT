881c043b2f9e2d1120ea4e1b9c11afc782e272f2
Rubén Valls <r.vallsaparici@gmail.com>
2022-11-14 15:53:34 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [topic, setTopic] = useState('dog');
+  const [number, setNumber] = useState('1');
+  const [gifs, setGifs] = useState([])
+  const inputRef = useRef(null);
+  const numberRef = useRef(null);
+
+  function Click(){
+    console.log(inputRef.current.value)
+    setTopic(inputRef.current.value)
+    setNumber(numberRef.current.value)
+  }
+
+  useEffect(() => {
+    const APIurl = `https://api.giphy.com/v1/gifs/search?api_key=WT6uDyLqdyizb6RFxmYVAszweIgWYXIh&q=${topic}&limit=${number}&offset=0&rating=g&lang=en`
+    fetch(APIurl)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gif = data.map(image => image.images.downsized_large.url)
+      if(gif[0]){
+        setGifs(gif)
+      }else{
+        window.alert('There is no GIF for your search. Try again.')
+      }
+    })
+  }, [topic, number]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>Select a topic and quantity for a random GIF/s</h1>
+        {
+          gifs.map(gif => <img src={gif}/>)
+        }
+        Topic: <input ref={inputRef} type="text" className="text-zone"></input>
+        Number of Gif<input ref={numberRef} type="number" min={1} max={100} className="text-zone"></input>
+        <button onClick={Click} className="text-zone">Generate!</button>
+      </section>
     </div>
   );
 }
