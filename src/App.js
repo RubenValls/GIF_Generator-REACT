53c628b49c90dfaf14c4e8ac70b933df9bc3dd42
Rubén Valls <r.vallsaparici@gmail.com>
2022-11-15 10:10:15 +0100

    Nav and footer start implement

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
 
   return (
-    <div className='container-fluid App-content'>
+    <div className='App-content'>
+      <div className='container-fluid'>
+        <h1 className='text-center'>NAV BAR</h1>
+      </div>
       <div className='container'>
         <GifRender></GifRender>
+      </div>
+      <div className='container-fluid'>
+        <h1 className='text-center'>FOOTER</h1>
       </div>
     </div>
   );
