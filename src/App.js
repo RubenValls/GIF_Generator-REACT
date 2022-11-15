c19d42b3872c05d7f35a3687dd13633cbc323c04
Rubén Valls <r.vallsaparici@gmail.com>
2022-11-15 13:14:37 +0100

    Final version

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import GifRender from './container/GifRender';
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from './container/pure/footer';
 import Nav from './container/pure/nav';
+import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <div className='App-content'>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Gif Generator</title>
+      </Helmet>
       <Nav/>
       <GifRender/>
       <Footer/>
